f9bf4d1f3100ff4087ee8c82976b53b522f32317
Your Name <your_email@example.com>
2026-02-28 01:50:59 +0530

    Added root route

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/events', eventsRoutes);
-
+app.get("/", (req, res) => {
+  res.send("Backend is running successfully 🚀");
+});
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
 });
